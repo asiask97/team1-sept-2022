5be4fd673b092e8626246b393837078d76952ea2
MelindaZhang2020 <melinda.zhang@phorest.com>
2022-09-08 23:27:57 +0100

    Create responsiable new hero image

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def favicon():
 if os.environ.get("DEVELOPMENT") == True :
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL")
 else:
-    uri = os.environ.get("DATABASE_URL")
+    uri = str(os.environ.get('DATABASE_URL'))
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri  

51159cd4590f3be12a879d0eda819043be2923b8
Asia <joannaskoc@gmail.com>
2022-09-09 12:07:41 +0100

    animation to homepage

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -4,6 +4,10 @@ document.addEventListener('DOMContentLoaded', function() {
         job.addEventListener('click', handler)
     })
     function handler(e){
+<<<<<<< HEAD
+=======
+        console.log(e.target.classList[4])
+>>>>>>> animation to homepage
         option=''
         if(e.target.innerHTML == 'Save Job'){
             e.target.innerHTML = 'Remove'
@@ -13,8 +17,14 @@ document.addEventListener('DOMContentLoaded', function() {
             option='remove'
         }
         var xhr = new XMLHttpRequest();
+<<<<<<< HEAD
         xhr.open('POST', '/jobposts/saved' + '?id=' + e.target.classList[3] + '&option=' + option, true); 
         xhr.send();
+=======
+        xhr.open('POST', '/jobposts/saved' + '?id=' + e.target.classList[4] + '&option=' + option, true); 
+        xhr.send();
+    
+>>>>>>> animation to homepage
     }
     
     const slidingOne = document.querySelector('.slide-in');
